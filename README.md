f229fa1a90af7f212ed7d5f19ccb108809e9b809
kbobrien <keith@mad-me.com>
2020-10-01 21:14:45 +0100

    Added some server stats

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Redis-Keyspace-Stats
+
+It's best practice to use
+
+
+## Usage
+
+```
+$ python redis_keyspace_stats.py --help
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -d DELIMITER   Keyspace delimeter used  (default :)
+  -H HOST        Redis host for connection
+  -P PORT        Redis port for connection  
+  -m MATCH       Match pattern to use for scaning keyspace
+  -s SCAN_COUNT  Number of items per scan command cursor (default 5000)  
+  -D DB          Redis database to use (default 0)
+  -t             Performs a test run on first 10 keys.  
+```
+
+
+
+
+## Docker
+
+To run the tool via docker you use the following:
+
+```
+docker run --rm redis-keyspace-stats:latest python redis_keyspace_stats.py --help
+```
+
+example:
+```
+docker run --rm redis-keyspace-stats:latest python redis_keyspace_stats.py -H host.docker.internal -P 36379 -t -m AH:*
+```
+
+*Note:* On Docker For Mac, you need to use the `host.docker.internal` hostname to access ports on the host machine.
+
+
+### Build
+
+To build the container use the following:
+```
+docker build -t redis-keyspace-stats .
+```
--- src/redis_keyspace_stats.py
+++ src/redis_keyspace_stats.py
@@ -1,16 +1,15 @@
 import redis
 import argparse
+import pprint
 
-parser = argparse.ArgumentParser(description='Scan all keys in redis and analyse first X chars.')
-parser.add_argument('-c', dest='char_size', action='store', type=int, help='Number of characters to prefix', default=3)
-parser.add_argument('-s', dest='scan_count', action='store', type=int, help='Number of items per scan command cursor', default=500)
-parser.add_argument('-P', dest='port', action='store', type=int, help='Redis port for connection', default=6379)
+parser = argparse.ArgumentParser(description='Scan all keys in redis database, extracting stats for keyspaces based on a delimeter.')
 parser.add_argument('-H', dest='host', action='store', type=str, help='Redis host for connection', default='localhost')
-parser.add_argument('-t', dest='test', action='store_true', help='Performs a test run on first 10,000 keys.', default=False)
-parser.add_argument('-d', dest='delimiter', action='store', type=str, help='Keyspace delimeter used', default=':')
-parser.add_argument('-m', dest='match', action='store', help='Match pattern to use for scaning keyspace', default='*')
-parser.add_argument('-D', dest='db', action='store', type=int, help='Redis database to use', default=0)
-
+parser.add_argument('-P', dest='port', action='store', type=int, help='Redis port for connection', default=6379)
+parser.add_argument('-D', dest='db', action='store', type=int, help='Redis database to use (default 0)', default=0)
+parser.add_argument('-d', dest='delimiter', action='store', type=str, help='Keyspace delimeter used (default :)', default=':')
+parser.add_argument('-s', dest='scan_count', action='store', type=int, help='Number of items per scan command cursor', default=500)
+parser.add_argument('-t', dest='test', action='store_true', help='Performs a test run on first 10 matching keys.', default=False)
+parser.add_argument('-m', dest='match', action='store', help='Match pattern to use for scaning keyspace, e.g. AH*', default='*')
 args = parser.parse_args()
 
 cache = redis.StrictRedis(host=args.host, port=args.port, db=args.db)
@@ -23,8 +22,11 @@ key_counts = dict()
 mem_sum = dict()
 key_item_counts = dict()
 key_type = dict()
+redis_mem_stats = cache.info(section='memory')
+redis_key_count = cache.info(section='keyspace')
 
 i = 0
+bytes = 0
 for key in cache.scan_iter(match=args.match, count=args.scan_count):
     key_str = key.decode()
     pipe = cache.pipeline()
@@ -54,6 +56,7 @@ for key in cache.scan_iter(match=args.match, count=args.scan_count):
         mem_sum[prefix] = key_info[0]
         key_type[prefix] = key_info[1].decode()
     i += 1
+    bytes += key_info[0]
 
     if i/10000 == 0:
         print("Gathered stats on {} keys...".format(i))
@@ -64,17 +67,24 @@ for key in cache.scan_iter(match=args.match, count=args.scan_count):
 
 
 print('')
-print(' Counts:')
+print('Redis keyspace stats for: {}:{}<{}>'.format(args.host, args.port, args.db))
 print('-------')
-print('Total: ', i)
-print('-------')
+print('Total Keys : ', i)
+print('Total Bytes: ', bytes)
+print('')
+print('Server stats:')
+print(' Keystats:', redis_key_count)
+print(' Used Memory:', redis_mem_stats['used_memory'])
+print(' Used Memory For Dataset:', redis_mem_stats['used_memory_dataset'])
+print(' Used Memory For Dataset:', redis_mem_stats['used_memory_dataset_perc'])
+print('------------')
 print('Keyspace Stats:')
-print('-------')
-print('{:<20}{:>10}{:>15}{:>25}{:>20}{:>35}{:>20}{:>20}'.format('Prefix', 'Type', 'NoKeys', 'TotalBytes', 'AvgBytesPerKey', 'TotalItems', 'AvgItemsPerKey', 'AvgBytesPerItem'))
+print('---------------')
+print('{:<20}{:>10}{:>15}{:>25}{:>20}{:>20}{:>20}{:>20}'.format('Prefix', 'Type', 'NoKeys', 'TotalBytes', 'AvgBytesPerKey', 'TotalItems', 'AvgItemsPerKey', 'AvgBytesPerItem'))
 
 for k in key_counts:
     if key_counts[k] > 0:
         avg_size = int(round(mem_sum[k]/key_counts[k]))
         avg_item_key = int(round(key_item_counts[k]/key_counts[k]))
         avg_item_size = int(round(mem_sum[k]/key_item_counts[k]))
-    print('{:<20}{:>10}{:>15}{:>25}{:>20}{:>35}{:>20}{:>20}'.format(k, key_type[k], key_counts[k], mem_sum[k], avg_size, key_item_counts[k], avg_item_key,avg_item_size))
+    print('{:<20}{:>10}{:>15}{:>25}{:>20}{:>20}{:>20}{:>20}'.format(k, key_type[k], key_counts[k], mem_sum[k], avg_size, key_item_counts[k], avg_item_key,avg_item_size))
